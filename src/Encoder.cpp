8f5aa790df17d1b21821147ac5286524edec6e8b
unknown <threezinedine@gmail.com>
2021-11-24 23:31:00 +0700

    update encode 1 string

--- src/Encoder.cpp
+++ src/Encoder.cpp
@@ -1,24 +1,51 @@
 #include<iostream>
 #include "Encoder.h"
 #include "Parent.h"
+#include <iterator>
 #include <map>
+#include <stdexcept>
 #include<string.h>
+#include<sstream>
+#include <string>
+#include <vector>
+#include "string_conversion.h"
+#include "read_file.h"
 using namespace std;
 
 Encoder::Encoder(string inputFiles, string outputFiles): Parent(inputFiles, outputFiles){
+    hashFunction = getHashFunction("conventions\\normal_character.txt", "conventions\\morse_character.txt");
 }
 
-map<string, string> Encoder::getHashFunction(string, string){
+map<string, string> Encoder::getHashFunction(string keysFile, string valuesFile){
     map<string, string> result;
-    result["a"] = "-..";
+    vector<string> keyStrings = read_file(keysFile);
+    vector<string> valueStrings = read_file(valuesFile);
+    auto itKey = keyStrings.begin();
+    auto itValue = valueStrings.begin();
+    while(itKey != keyStrings.end()){
+        result[*itKey] = *itValue;
+        itKey ++;
+        itValue ++;
+    }
     return result;
 }
 
-string Encoder::transform(){
+string Encoder::transform(string s){
     time.start();
     string result = "";
-    cout << "Encode completely";
-    cout << endl << "Hello World";
+    int i = 0;
+    do{
+        if(s[i] == ' '){
+            result += "/";
+        }
+        else{
+            stringstream ss;
+            ss << s[i];
+            result += hashFunction[ss.str()] + " ";
+        }
+        i++;
+    }
+    while(s[i] != '\0');
     time.end();
     return result;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,19 @@
 #include "Encoder.h"
+#include "read_file.h"
 #include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include<string.h>
 using namespace std;
 
 int main(){
-    Encoder* test = new Encoder("\\Thao\\C++\\src", "\\Thao\\C++\\bin");
-    test->transform();
+    Encoder* test = new Encoder("inputs/test.txt", "outputs/output.txt");
+    string s;
+    cout << "Enter your string: ";
+    getline(cin, s);
+    string result = test->transform(s);
+    cout << result << endl;
     test->printStatics(); 
     return 0;
 }
