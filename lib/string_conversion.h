44b1a46cf88d574ce392c840f46f80515a6ebccb
ndanglan <nbh381891@gmail.com>
2021-11-25 18:12:09 +0700

    test

--- lib/string_conversion.h
+++
@@ -1,8 +0,0 @@
-#pragma once 
-#include<iostream>
-#include<string.h>
-#include<string>
-using namespace std;
-
-
-string toString(const char* characters);
--- lib/string_divide.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<string.h>
-#include<vector>
-#include<iostream>
-
-std::vector<std::string> stringSplit(std::string, char);
